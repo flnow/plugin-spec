b7447764e041f04905114d6ed26fb2a57a2410a6
Sirius Guo <guotuo1024@gmail.com>
2019-07-25 16:33:51 +0800

    Add prophase code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/flnow/plugin-spec
+
+go 1.12
---
+++ spec.go
@@ -0,0 +1,28 @@
+package spec
+
+// Response of flnow plugin spec for Golang
+type Response struct {
+	Result  bool        `json:"result"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+	Raw     interface{} `json:"raw"`
+}
+
+// Field to descript field of a Form
+type Field struct {
+	Display     string `json:"display"`
+	Name        string `json:"name"`
+	ShortName   string `json:"short"`
+	EnvName     string `json:"env"`
+	Default     string `json:"default"`
+	Description string `json:"description"`
+	Value       string `json:"value"`
+	Required    bool   `json:"required"`
+	Type        string `json:"type"`
+	Array       bool   `json:"array"`
+}
+
+// Form for flnow plugin UI display
+type Form struct {
+	Fields []Field `json:"fields"`
+}
